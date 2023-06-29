584ffc18ef031f8942ee72b31024978f3b7f7112
IA777 <qa.hometasks@gmail.com>
2023-06-30 01:32:04 +0300

    Homework

--- Hometasks/Hometask3/Задание2/lesson3_task2.py
+++ Hometasks/Hometask3/Задание2/lesson3_task2.py
@@ -1,1 +1,25 @@
+from smartphone_model import Smartphone
+
 catalog = []
+
+smart_1 = Smartphone("Samsung", "A10", "+37124860889")
+catalog.append(smart_1)
+
+smart_2 = Smartphone("Nokia", "3310", "+49454688221")
+catalog.append(smart_2)
+
+smart_3 = Smartphone("Ericksson", "T28s", "+790522555445")
+catalog.append(smart_3)
+
+smart_4 = Smartphone("Apple", "4s", "+37028556674")
+catalog.append(smart_4)
+
+smart_5 = Smartphone("One Plus", "One", "+86266579855")
+catalog.append(smart_5)
+
+    
+
+print("Каталог телефонов:")
+for phone in catalog:
+    print(f"{phone.ph_brand} - {phone.ph_mod} - {phone.ph_num}")
+
---
+++ Hometasks/Hometask3/Задание2/smartphone_model.py
@@ -0,0 +1,34 @@
+class Smartphone:
+
+    def __init__(self, brand, model, num):
+        self.ph_brand = brand
+        self.ph_mod = model
+        self.ph_num = num
+
+
+
+
+
+
+
+# class Smartphone:
+
+#     def __init__(self, brand, model, num):
+#        self.ph_brand = brand
+#        self.ph_mod = model
+#        self.ph_num = num
+
+#     def brandIs(self):
+#         print("Марка телефона: ", self.ph_brand)
+        
+#     def modIs(self):
+#         print("Модель телефона: ", self.ph_mod)
+
+#     def numIs(self):
+#         print("Номер телефона: ", self.ph_num) 
+
+# my_phone = Smartphone("Samsung", "A10", "+37124860889")
+# my_phone.brandIs()
+# my_phone.modIs()
+# my_phone.numIs()
+
